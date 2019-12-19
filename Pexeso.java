f6df6dbd1599c30c7b9f5d291cf6efd7276f6858
Filip Plevjak <54470003+filipPlevjak@users.noreply.github.com>
2019-12-19 22:50:57 +0100

    Update Pexeso.java

--- Pexeso.java
+++ Pexeso.java
@@ -1,69 +1,60 @@
 package hra;
-
-
- 
 import hraciaPlocha.Panel;
-import java.awt.event.*;
-import javax.swing.*;
-import java.util.*;
 import hraciaPlocha.KoniecHry;
-import hraciaPlocha.Menu;
 import hraciaPlocha.Skore;
+import java.util.Collections;
+import java.util.Arrays;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class Pexeso extends JFrame implements ActionListener {
 
-    
-    private static final String DEFAULT_IMAGE_FOLDER = "/obrazky/obal.jpg";
-    private int MAX_OBRAZKOV = 40;
-    private int MAX_STLACENE_OBRAZKY = 2;
-    private int POCET_PAROV_PEXESA = 20;
-    private int CAKACI_CAS = 500;
-    
+    private final String DEFAULT_IMAGE_FOLDER = "/obrazky/obal.jpg";
+    private final int MAX_OBRAZKOV = 40;
+    private final int MAX_STLACENE_OBRAZKY = 2;
+    private final int POCET_PAROV_PEXESA = 20;
+    private final int CAKACI_CAS = 500;
     private final JButton[] tlacidloPexeso = new JButton[MAX_OBRAZKOV];
     private final int[] stlaceneKarty = new int[MAX_STLACENE_OBRAZKY];
-    private final Karty[] karty = new Karty[MAX_OBRAZKOV];
-    
     private final ImageIcon zadnaStrana;
-    private final Panel panel;
-
- 
-
-    private final Postava postava1;
-    private final Postava postava2;
     private int pocetStlacenychKariet = 0;
     private int pocetUhadnutychkariet = 0;
     private int hracNaRade = 1;
     private int kliknutaPoziciaPexesa;
-    private boolean parPexesa = false;
     private boolean opakujTah;
-    private int body1,body2;
-    
+    private final Karta[] karta = new Karta[MAX_OBRAZKOV];
     private Skore skore;
+    private final Panel panel;
+    private final Hrac []hraci ;
 
     /*
      Konštruktor nám vytvára pexeso, obal,karty,panel a ukladá do súboru
     stav hry
      */
-    public Pexeso(Postava postava1, Postava postava2) {
-        
-        //
-       
-        this.postava1 = postava1;
-        this.postava2 = postava2;
+    public Pexeso(Hrac []paHraci) {
+    this.hraci = paHraci;
 
-        panel = new Panel(postava1, postava2);
-        skore = new Skore(postava1,postava2);
+
+        panel = new Panel(hraci);
+        skore = new Skore(hraci);
         zadnaStrana = new ImageIcon(Pexeso.class.getResource(DEFAULT_IMAGE_FOLDER));
         kliknutaPoziciaPexesa = -1;
 
-
-       // for (int i = 0; i < MAX_OBRAZKOV; i++) {
-        //    karty[i] = new Karty(null,0,false,i);
-       // }
+        int j = 0;
+        for (int i = 0; i < MAX_OBRAZKOV; i+=2) {
+            karta[i] = new Karta(j%20);
+            karta[i].setCisloObrazku(j);
+            karta[i+1] = new Karta(j%20);
+            karta[i+1].setCisloObrazku(j);
+            j++;
+        }
         
         //premiesa vsetky ie karty
-        Collections.shuffle(Arrays.asList(karty));
+        Collections.shuffle(Arrays.asList(karta));
 
         for (int i = 0; i < MAX_OBRAZKOV; i++) {
             tlacidloPexeso[i] = new JButton();
@@ -86,77 +77,77 @@ public class Pexeso extends JFrame implements ActionListener {
             return;
         }
 
-        JButton tlačidlo = (JButton) event.getSource();
+        JButton tlacidlo = (JButton) event.getSource();
 
         for (int i = 0; i < MAX_OBRAZKOV; i++) {
-            if (((tlačidlo).equals(tlacidloPexeso[i])) && (karty[i].getNajdenyPar() == false) && (i != kliknutaPoziciaPexesa) ) {//
+            if ((tlacidlo.equals(tlacidloPexeso[i])) && (!karta[i].getNajdenyPar()) && ( i != kliknutaPoziciaPexesa)) {
                 kliknutaPoziciaPexesa = i;
-                tlačidlo.setIcon(karty[i].getObrazok());
+                tlacidlo.setIcon(karta[i].getObrazok());
 
-                if (pocetStlacenychKariet == 0) stlaceneKarty[0] = i;
-                if (pocetStlacenychKariet == 1) stlaceneKarty[1] = i;
+                if (pocetStlacenychKariet == 0)
+                    stlaceneKarty[0] = i;
+                
+                if (pocetStlacenychKariet == 1)
+                    stlaceneKarty[1] = i;
+                
                 pocetStlacenychKariet++;
+                break;
             }
         }
+
         if (pocetStlacenychKariet == MAX_STLACENE_OBRAZKY) {
-            //tvoria par
-            if ((karty[stlaceneKarty[0]].getCisloObrazku()) == (karty[stlaceneKarty[1]].getCisloObrazku())) {
-                parPexesa = true;
+            // tvoria par
+            if ((karta[stlaceneKarty[0]].getCisloObrazku()) == (karta[stlaceneKarty[1]].getCisloObrazku())) {
                 pocetStlacenychKariet = 0;
-                karty[stlaceneKarty[0]].setNajdenyPar(true);
-                karty[stlaceneKarty[1]].setNajdenyPar(true);
+                karta[stlaceneKarty[0]].setNajdenyPar(true);
+                karta[stlaceneKarty[1]].setNajdenyPar(true);
                 kliknutaPoziciaPexesa = -1;
                 
                 pocetUhadnutychkariet++;
-                setBody();
-                skore = new Skore(postava1,postava2);
+                setBody(hraci);
+                skore = new Skore(hraci);
                 koniecHry(pocetUhadnutychkariet);
                 opakujTah = true;
 
-            }
-            
-     //netvoria pár
-            else {
-           
+            } else {
+                //netvoria pár
+
                 Pexeso hra = this;
+
                 //cakanie po neúpešnom otočenom páre
                 //karticky sa vrátia do pôvodného tvaru
                 Thread thread = new Thread(() -> {
-                    try {
-                        Thread.sleep(CAKACI_CAS);
-                    } catch (Exception exe) {
-                    }
-                    hra.zakryPar();
-                });
+                        try {
+                            Thread.sleep(CAKACI_CAS);
+                        } catch (Exception exe) {
+                        }
+                        hra.zakryPar();
+                    });
+
                 thread.start();
                 opakujTah = false;
             }
-            if (opakujTah == false) {
-            hracNaRade++;
+
+            if (!opakujTah) {
+                hracNaRade++;
             }
             
             hracNaRade = getHrac(hracNaRade); 
             zmenFarbu();
-            parPexesa = false;
-
         }
     }
 
-//priradí bod danemu hračovi, podla toho kto je na rade
-private void setBody(){
-           if ((hracNaRade % 2) == 0) {
-                    body2++;
-                    postava2.setBody(body2);
-                    panel.zmenBody(postava2, 2);
-                     
-                 }
-                 else
-                 {  body1++;
-                     postava1.setBody(body1);
-                    panel.zmenBody(postava1, 1);
-                 }
+    // priradí bod danemu hračovi, podla toho kto je na rade
+    private void setBody(Hrac[] hraci){
+        if ((hracNaRade % 2) == 0) {
+            hraci[1].setBody(1);
+            panel.zmenBody(hraci[1], 2);
+        } else { 
+            hraci[0].setBody(1);
+            panel.zmenBody(hraci[0], 1);
+        }
 }
- //zakryje karty ak uhádnutý pár nebol správny
+    // zakryje karty ak uhádnutý pár nebol správny
     private void zakryPar() {
         tlacidloPexeso[stlaceneKarty[0]].setIcon(zadnaStrana);
         tlacidloPexeso[stlaceneKarty[1]].setIcon(zadnaStrana);
@@ -165,8 +156,8 @@ private void setBody(){
     }
 
  //urcuje ktory hrac je na rade
-    private int getHrac(int naRade) {
-        if ((naRade % 2) != 0) {
+    private int getHrac(int hracNaRade) {
+        if ((hracNaRade % 2) != 0) {
             return 1;
         } else {
             return 2;
@@ -186,25 +177,17 @@ private void setBody(){
     }
 
 //koniec hry pri uhádnutí všetkých kariet
-    private void koniecHry(int pocet) {
-        if (pocet == POCET_PAROV_PEXESA) {
+    private void koniecHry(int pocetUhadnutych) {
+        KoniecHry koniec;
+        if (pocetUhadnutych == POCET_PAROV_PEXESA) {
             
-            if(postava1.getBody() >  postava2.getBody()){
-             KoniecHry koniec = new KoniecHry(postava1.getMeno(),postava1.getBody());
-             koniec.setVisible(true);
-            }
-            else{
-            
-             KoniecHry koniec = new KoniecHry(postava2.getMeno(),postava2.getBody());
-             koniec.setVisible(true);
-            }      
+            if(hraci[0].getBody() >  hraci[1].getBody()){
+                koniec = new KoniecHry(hraci[0].getMeno(),hraci[0].getBody());
+            } else{
+                koniec = new KoniecHry(hraci[1].getMeno(),hraci[1].getBody());
+            }   
+            koniec.setVisible(true);
        }
     }
-
-        public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> {
-        new Menu().setVisible(true);
-        });
-    }
-    
+  
 }
