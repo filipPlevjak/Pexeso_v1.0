27b098d90827f3ca164e6ca289732de755997084
Filip Plevjak <54470003+filipPlevjak@users.noreply.github.com>
2019-12-19 22:48:58 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package hra;
+
+import hraciaPlocha.Menu;
+
+public class Main {
+    
+            public static void main(String[] args) {
+                java.awt.EventQueue.invokeLater(() -> {
+                new Menu().setVisible(true);
+            });
+    }
+}
